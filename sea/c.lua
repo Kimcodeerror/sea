55dfb0ffd0db9f790f2d669b16c90b817fd2309c
Kimcodeerror <127507481+Kimcodeerror@users.noreply.github.com>
2023-03-10 20:06:38 +0700

    Add files via upload

---
+++ sea/c.lua
@@ -0,0 +1,10 @@
+Citizen.CreateThread(function()
+	while true do
+		Wait(0)
+		Citizen.InvokeNative(0xC54A08C85AE4D410, 3.0)     
+	end
+end) 
+
+---0.0 = ปกติ  
+--- 1.0f = สงบ
+--- 3.0f = เข้มข้นจริงๆ
